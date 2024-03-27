026c62c5a3d36387859bd013f3c84a71b3a2a291
Vic <162835558+BarkByteAdventures@users.noreply.github.com>
2024-03-27 16:09:11 +0100

    Add files via upload

---
+++ Console/block_letter.js
@@ -0,0 +1,24 @@
+/*Create a JavaScript program that displays your initials in block letters.
+
+First, start your program with a comment (single line or multi-line) that says a fun fact about yourself.
+
+Then, create your block letters with your initials.
+
+For example, if your name is Billie Eilish, your initials would be BE, and your block letters would look like this:
+
+BBBB    EEEEE
+B   B   E
+B   B   E
+BBBB    EEEE
+B   B   E
+B   B   E
+BBBB    EEEEE
+*/
+
+console.log("V    V RRRRR"); 
+console.log("V    V R    R");
+console.log("V    V R    R");
+console.log(" V  V  RRRRR");
+console.log(" V  V  RRR");
+console.log(" V  V  R RR");
+console.log("  VV   R  RR");
---
+++ Console/receipt.js
@@ -0,0 +1,48 @@
+/*In this exercise, you are going to recreate a receipt of a purchase from the last store or restaurant that you went to... using JavaScript! 🧾
+
+Make sure to check your pockets and wallet if you forgot.
+
+Use console.log() methods to print the following info:
+
+    Name of the store/restaurant.
+    A row that says "Order Details".
+    A row with column names for "Item", "Quantity", and "Price".
+    At least two items that were purchased.
+    The total price for the purchase.
+
+The output should look like:
+
+==================================
+         McDONALD'S RECEIPT       
+----------------------------------
+           Order Details          
+----------------------------------
+Item           Quantity   Price($)
+----------------------------------
+🍔 Burger       1          2.99   
+🍟 Fries        1          2.49   
+🥤 Soda         1          1.99   
+----------------------------------
+Total                      7.47   
+==================================
+
+When you are done, take a screenshot and click the Twitter icon to share your receipt!
+*/
+
+console.log("========================================");
+console.log("             BUYA RAMEN FACTORY         ");
+console.log("----------------------------------------");
+console.log("               Order Details            ");
+console.log("----------------------------------------");
+console.log("Item             Quantity     Price(€)  ");
+console.log("----------------------------------------");
+console.log("Miso Cucumber       1           6.00    ");
+console.log("Crispy Duck Ramen   1          18.50    ");
+console.log("Mochi Ice Cream     1           5.00    ");
+console.log("Melon Water         1           3.65    ");
+console.log("----------------------------------------");
+console.log("Total                          33.15€   ");
+console.log("========================================");
+
+
+
